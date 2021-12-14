ebc74cf266573d71f561541ad27cf9ab40bb9683
Dictatura <dictatura@yeah.net>
2021-12-14 20:23:54 +0800

    config 同时合并actor端和critic端

--- utils/config.py
+++ utils/config.py
@@ -7,36 +7,19 @@ device = torch.device("cpu")
 
 action_space = 4
 
-# actor net
-actor_dict = {
-    'in_size': 1, 'out_size': action_space,
+# net
+net_dict = {
+    'in_size': 1,
     'emb_num': 4, 'emb_out_size': 4,
     'conv1_in_size': 4, 'conv1_out_size': 16, 'conv1_kernel_size': 3, 'conv1_stride': 1,
     'conv2_in_size': 16, 'conv2_out_size': 8, 'conv2_kernel_size': 2, 'conv2_stride': 1,
     'lstm_in_size': 8, 'lstm_hidden_size': 8, 'lstm_num_layers': 2,
-    'fc1_in_size': 8, 'fc1_out_size': 8,
-    'fc2_in_size': 8,
+    'fc1_a_in_size': 8, 'fc1_a_out_size': 8, 'fc2_a_in_size': 8, 'fc2_a_out_size': action_space,
+    'fc1_c_in_size': 8, 'fc1_c_out_size': 4, 'fc2_c_in_size': 4, 'fc2_c_out_size': 1,
     'lr': 0.00001
 }
 
-for key, value in actor_dict.items():
+for key, value in net_dict.items():
     parser.add_argument(key, action='store_const', const=value)
 
-actor_param = parser.parse_args()
-
-# critic net
-critic_dict = {
-    'in_size': 1, 'out_size': 1,
-    'emb_num': 4, 'emb_out_size': 4,
-    'conv1_in_size': 4, 'conv1_out_size': 16, 'conv1_kernel_size': 3, 'conv1_stride': 1,
-    'conv2_in_size': 16, 'conv2_out_size': 8, 'conv2_kernel_size': 2, 'conv2_stride': 1,
-    'lstm_in_size': 8, 'lstm_hidden_size': 8, 'lstm_num_layers': 2,
-    'fc1_in_size': 8, 'fc1_out_size': 4,
-    'fc2_in_size': 4,
-    'lr': 0.00001
-}
-
-for key, value in critic_dict.items():
-    parser.add_argument(key, action='store_const', const=value)
-
-critic_param = parser.parse_args()
+net_param = parser.parse_args()
